7c8b2e921a3fb41f3589703b0204bfcecca0973c
Juliana Karoline <julianaklulo@gmail.com>
2020-07-31 00:29:27 -0300

    Aula 2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
 
 function Menu() {
-    return (
-        <nav className="Menu">
-            <a href="/">
-                <img className="Logo" src={Logo} alt="AluraFlix Logo" />
-            </a>
-            <Button as="a" href="/">
-                Novo vídeo
-            </Button>
-        </nav>
-    );
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="AluraFlix Logo" />
+      </Link>
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
